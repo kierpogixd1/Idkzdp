1c167b3ea82059a9383525e77cc54522492dc757
Phoung <146567960+kierpogixd1@users.noreply.github.com>
2024-07-02 19:25:48 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/chrsschrs/antilocks/main/aa"))()
+--// real fake animation zombie don't use any animation it will looks real also permanent animation
+
+local Players = game:GetService("Players")
+
+local function animateCharacter()
+    while true do
+        local character = Players.LocalPlayer.Character
+        if character then
+            local animate = character:FindFirstChild("Animate")
+            if animate then
+                animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616168032"
+                animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616163682"
+            end
+            local humanoid = character:FindFirstChildOfClass("Humanoid")
+            if humanoid then
+                humanoid.Jump = false
+            end
+        end
+        wait(1)
+    end
+end
+
+animateCharacter()
